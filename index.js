2411cc97f709f279a5cc8201057fc12b9a89bf85
Swag <john.ifert.miller@gmail.com>
2022-11-18 17:02:50 -0500

    So far so good day 1 of work

---
+++ index.js
@@ -0,0 +1,48 @@
+// Global constants
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateHTML = require("./lib/generateHTML")
+
+// Questions array
+function questions(){
+inquirer
+.prompt([
+    {
+        type: "input", 
+        name: "name", 
+        message: "What is employee's name?"
+    },
+    {
+        type: "input", 
+        name: "id", 
+        message: "What is employee's ID?"
+    },
+    {
+        type: "input", 
+        name: "email", 
+        message: "What is employee's email?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is employee's role?",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    }
+])
+.then((answers) => {
+const createHTML = generateHTML(answers);
+const fileName = "./dist/index.html";
+fs.appendFile(fileName, createHTML, (error) => 
+error ? console.error(error) : console.log("Saved!"));
+})
+}
+
+function init() {
+    questions();
+}
+
+init();
